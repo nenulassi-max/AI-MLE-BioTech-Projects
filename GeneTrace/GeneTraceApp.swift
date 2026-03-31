c3d2a67ef1990846dcfe1da47f11756892b7e424
Laasya Vibha Machiraju <nenulassi@gmail.com>
2026-03-31 16:07:03 -0700

    Initial Commit

---
+++ GeneTrace/GeneTraceApp.swift
@@ -0,0 +1,32 @@
+//
+//  GeneTraceApp.swift
+//  GeneTrace
+//
+//  Created by Minnamma on 3/31/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GeneTraceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ GeneTrace/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GeneTrace
+//
+//  Created by Minnamma on 3/31/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
